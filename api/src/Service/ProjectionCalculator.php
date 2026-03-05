d4647d6ce1ac8817d0aac00d68f8b30a0886e5c6
Brendan Tuckerman <brendan.tuckerman@awm.gov.au>
2026-03-06 10:16:27 +1100

    Added basic calculations

--- api/src/Service/ProjectionCalculator.php
+++ api/src/Service/ProjectionCalculator.php
@@ -5,9 +5,33 @@ namespace App\Service;
 
 class ProjectionCalculator
 {
-
+    /**
+     * @param $income int
+     * @param $expendture int
+     *
+     * @return int difference between income and expenditure
+     */
     public function calculateIncomeDifference(int $income, int $expenditure): int
     {
         return ($income - $expenditure);
     }
+
+
+    public function calculateSavingsRate(int $income, int $expenditure): int
+    {
+        return (int) ceil(($income - $expenditure) / $income * 100);
+    }
+
+    /**
+     * @param int $investyment amount in $
+     * @param int $interestRate as an annual %
+     *
+     * @return int diff between income and expenditure
+     */
+    public function calculateAnnualInterest(int $investmentAmount, int $interestRate)
+    {
+        return (int) ($investmentAmount * ($interestRate / 100));
+    }
+
+
 }
--- api/tests/ProjectionCalculatorTest.php
+++ api/tests/ProjectionCalculatorTest.php
@@ -13,6 +13,44 @@ class ProjectionCalculatorTest extends TestCase
         $income = 1000;
         $outgoings = 500;
         $result = $calculator->calculateIncomeDifference($income, $outgoings);
-        $this->assertTrue($result === 500);
+        $this->assertSame(500, $result);
+
+        // Large numbers
+        $result_2 = $calculator->calculateIncomeDifference(1000000, 750000);
+        $this->assertSame(250000, $result_2);
+
+        // Expenditure exceeds income
+        $result_3 = $calculator->calculateIncomeDifference(500, 800);
+        $this->assertSame(-300, $result_3);
+    }
+
+    public function testSavingsRate(): void
+    {
+        $calculator = new ProjectionCalculator();
+        $income_1 = 1000;
+        $outgoings_1 = 500;
+        $result_1 = $calculator->calculateSavingsRate($income_1, $outgoings_1);
+        $this->assertSame(50, $result_1);
+
+        // Non-round number: ceil(8496/8985*100) = ceil(94.56) = 95
+        $income_2 = 8985;
+        $outgoings_2 = 489;
+        $result_2 = $calculator->calculateSavingsRate($income_2, $outgoings_2);
+        $this->assertSame(95, $result_2);
+
+        // Spending more than income: ceil(-100/900*100) = ceil(-11.11) = -11
+        $income_3 = 900;
+        $outgoings_3 = 1000;
+        $result_3 = $calculator->calculateSavingsRate($income_3, $outgoings_3);
+        $this->assertSame(-11, $result_3);
+    }
+
+    public function testAnnualInterest(): void
+    {
+        $calculator = new ProjectionCalculator();
+        $investment_1 = 1000;
+        $interestRate_1 = 5;
+        $result_1 = $calculator->calculateAnnualInterest($investment_1, $interestRate_1);
+        $this->assertSame(50, $result_1);
     }
 }
